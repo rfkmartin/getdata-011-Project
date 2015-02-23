a514ad8f4263a7d678fac05b267a1ee31390c562
Robert F.K. Martin <rfkmartin@gmail.com>
2015-02-23 14:38:19 -0600

    finished

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # getdata-011-Project
+
+* how to run
+> source("run_analysis.R")
+
+* see comments in code for description of process
+
+* decision on which variables to pick
+
+Although many variable names contain mean or std, I selected only those that had the string "mean()" or "std()" in them since variable names without the parentheses denote some other type of mean calculation.
+
+* decriptive variable names
+
+I assumed the original researchers used good, descriptive variable names for their data and kept them as close to the original as possible. The only alterations I made were getting rid of parenthese and hyphens
+
+can be read by
+> data<-read.table("final.txt",header=TRUE)
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
+library("dplyr")
 ## read in features
-feats<-read.table("../features.txt")
+feats<-read.table("UCI\ HAR\ Dataset/features.txt")
 ## select features with mean() and std()
 idx.mean<-grep("mean\\(\\)",feats$V2)
 idx.std<-grep("std\\(\\)",feats$V2)
@@ -7,9 +8,9 @@ idx<-union(idx.mean,idx.std)
 idx<-idx[order(idx)]
 
 ## read in test data
-test.subject =read.table("../test/subject_test.txt")
-test.X       =read.table("../test/X_test.txt")
-test.y       =read.table("../test/y_test.txt")
+test.subject =read.table("UCI HAR Dataset/test/subject_test.txt")
+test.X       =read.table("UCI HAR Dataset/test/X_test.txt")
+test.y       =read.table("UCI HAR Dataset/test/y_test.txt")
 ## merge these together
 test<-cbind(test.subject,test.y,test.X)
 ## clear up some space
@@ -18,9 +19,9 @@ remove(test.X)
 remove(test.y)
 
 ## read in training data
-train.subject=read.table("../train/subject_train.txt")
-train.X      =read.table("../train/X_train.txt")
-train.y      =read.table("../train/y_train.txt")
+train.subject=read.table("UCI HAR Dataset/train/subject_train.txt")
+train.X      =read.table("UCI HAR Dataset/train/X_train.txt")
+train.y      =read.table("UCI HAR Dataset/train/y_train.txt")
 ## merge these together
 train<-cbind(train.subject,train.y,train.X)
 ## clear up some space
@@ -38,9 +39,22 @@ remove(test)
 names(data)<-c("Subject.ID","Activity.ID",as.character(feats$V2))
 idx=c(1,2,idx+2)
 data<-data[idx]
+## clean up variable names
+old.names=names(data)
+data.names<-gsub("-mean\\(\\)",".mean",old.names)
+data.names<-gsub("-std\\(\\)",".std",data.names)
+data.names<-gsub("-X",".X",data.names)
+data.names<-gsub("-Y",".Y",data.names)
+data.names<-gsub("-Z",".Z",data.names)
+names(data)<-data.names
 
 ## read activity
-acts<-read.table("../activity_labels.txt")
+acts<-read.table("UCI HAR Dataset/activity_labels.txt")
 names(acts)<-c("Activity.ID","Activity")
 ## label activities with text labels
-mean.data<-merge(acts,data)
+data<-merge(acts,data)
+## remove redundant Activity.ID
+data<-data[,c(2:69)]
+
+final<-data %>% group_by(Activity,Subject.ID) %>% summarise_each(funs(mean))
+write.table(final,file="final.txt",row.name=FALSE)
